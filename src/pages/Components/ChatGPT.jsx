f0af3294cb64b2fc8925458cf418cfb79cc861a4
Anth <sol9001@gmail.com>
2023-06-02 11:15:31 -0400

    updates

--- src/pages/Components/ChatGPT.jsx
+++ src/pages/Components/ChatGPT.jsx
@@ -3,7 +3,7 @@ import { createWindow } from '../Content/modules/closeWindow';
 class ChatGPT extends React.Component {
 
     render() {
-        createWindow("https://chat.openai.com/", 800, 430, 0, 615)
+        createWindow("https://chat.openai.com/", 830, 400, 0, 615)
 
         return (
             <div />
