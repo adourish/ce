1c2ea2cbcd6d050d5c0666ab9786f155f37dccae
Anth <sol9001@gmail.com>
2023-06-02 21:41:39 -0400

    added switch for the different commands

--- src/pages/Terminal/Console.jsx
+++ src/pages/Terminal/Console.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Console.css';
-
 function Console({ inputText, setInputText, content, setContent }) {
   const handleInputChange = (event) => {
     setInputText(event.target.value);
@@ -8,8 +7,64 @@ function Console({ inputText, setInputText, content, setContent }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setContent([...content, inputText]);
+    const command = inputText.trim();
+    console.log('test submit:', command);
+    if (command.startsWith('/')) {
+      const commandParts = command.split(' ');
+      const cmd = commandParts[0];
+      const text = commandParts.slice(1).join(' ');
+
+      switch (cmd) {
+        case '/note':
+          handleNoteCommand(text);
+          break;
+        case '/chat':
+          handleChatCommand(text);
+          break;
+        case '/task':
+          handleTaskCommand(text);
+          break;
+        case '/cal':
+          handleCalCommand(text);
+          break;
+        default:
+          handleUnknownCommand();
+      }
+    } else {
+      // Handle regular input
+      setContent([...content, inputText]);
+    }
+
     setInputText('');
+  };
+
+  const handleNoteCommand = (text) => {
+    // Handle "/note" command
+    // Example: add note logic
+    console.log('Note:', text);
+  };
+
+  const handleChatCommand = (text) => {
+    // Handle "/chat" command
+    // Example: add chat logic
+    console.log('Chat:', text);
+  };
+
+  const handleTaskCommand = (text) => {
+    // Handle "/task" command
+    // Example: add task logic
+    console.log('Task:', text);
+  };
+
+  const handleCalCommand = (text) => {
+    // Handle "/cal" command
+    // Example: add calendar logic
+    console.log('Calendar:', text);
+  };
+
+  const handleUnknownCommand = () => {
+    // Handle unknown command
+    console.log('Unknown command');
   };
 
   return (
