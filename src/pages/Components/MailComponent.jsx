2e991dcb60bbb3c38b329193021403e8517595b4
Anth <sol9001@gmail.com>
2023-08-20 17:14:07 -0400

    resize

--- src/pages/Components/MailComponent.jsx
+++ src/pages/Components/MailComponent.jsx
@@ -3,7 +3,7 @@ import { createWindow } from '../Content/modules/closeWindow';
 class Mail extends React.Component {
 
     render() {
-        createWindow("https://mail.google.com/mail/u/0/#inbox", 750, 430, 0, 550, true, 'popup')
+        createWindow("https://mail.google.com/mail/u/0/#inbox", 1000, 430, 0, 550, true, 'popup')
 
         return (
 
